cb4b260384ac75088e2974d5b9b78e18e665d405
gazbert <gareth.jon.lynch@gmail.com>
2016-09-19 22:25:05 +0100

    Heroes tutorial - 7 (Search)

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,8 +4,8 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule}    from '@angular/http';
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import {InMemoryWebApiModule} from 'angular2-in-memory-web-api';
+import {InMemoryDataService}  from './in-memory-data.service';
 
 import {AppComponent} from "./app.component";
 import {HeroDetailComponent} from "./hero-detail.component";
@@ -13,10 +13,11 @@ import {HeroesComponent} from "./heroes.component";
 import {HeroService} from "./hero.service";
 import {routing} from "./app.routing";
 import {DashboardComponent} from "./dashboard.component";
+import {HeroSearchComponent} from "./hero-search.component";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService), routing],
-    declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent],
+    declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroSearchComponent, HeroDetailComponent],
     providers: [HeroService],
     bootstrap: [AppComponent]
 })
