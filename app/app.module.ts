8789812c63f0987fef8bdf5340cbffd88edd2c20
gazbert <gareth.jon.lynch@gmail.com>
2016-09-19 21:16:43 +0100

    Heroes tutorial - 7 (CRUD)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,12 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
+import {HttpModule}    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import {AppComponent} from "./app.component";
 import {HeroDetailComponent} from "./hero-detail.component";
 import {HeroesComponent} from "./heroes.component";
@@ -9,7 +15,7 @@ import {routing} from "./app.routing";
 import {DashboardComponent} from "./dashboard.component";
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, routing],
+    imports: [BrowserModule, FormsModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService), routing],
     declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent],
     providers: [HeroService],
     bootstrap: [AppComponent]
