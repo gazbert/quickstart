da8b82b343ea76208b5e3d507bcff91f5486d0c1
gazbert <gareth.jon.lynch@gmail.com>
2016-11-19 13:48:26 +0000

    Merged in latest from upstream
    * Bumped up to Angular 2.2.0

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app.component';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {DebugElement, NO_ERRORS_SCHEMA} from '@angular/core';
 
 ////////  SPECS  /////////////
 describe('AppComponent', function () {
@@ -12,24 +12,24 @@ describe('AppComponent', function () {
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
-   TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('h1'));
+      });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
-  });
 
   it('should create component', () => expect(comp).toBeDefined() );
 
   it('should have expected <h1> text', () => {
     fixture.detectChanges();
     const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+    expect(h1.innerText).toMatch(/Tour/i,
+      '<h1> should say something about "Tour"');
   });
 });
