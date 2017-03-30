d0c5864ed899945f4fe6d01848d9c7286607c651
gazzbert <gareth.jon.lynch@gmail.com>
2017-03-30 11:34:07 +0100

    Upgraded to Angular 4

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -12,7 +12,7 @@
         map: {
 
             // our app is within the app folder
-            app: 'app',
+            'app': 'app',
 
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
